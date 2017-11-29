7e86f1b334eaeebe9bdc6d8b90fb811da9cc71a0
Felix Palmen <felix@palmen-it.de>
2017-11-29 10:52:29 +0100

    basic gameplay

--- src/board.s
+++ src/board.s
@@ -3,16 +3,35 @@
 .exportzp board
 .export board_init
 .export board_addpiece
+.export board_up
+.export board_down
+.export board_left
+.export board_right
+.export board_step
+
+DIR_UP		= 1
+DIR_DOWN	= 2
+DIR_LEFT	= 3
+DIR_RIGHT	= 4
 
 .zeropage
 
 board:		.res	$10
 tmppos:		.res	1
+direction:	.res	1
+moving:		.res	1
+combined:	.res	4
+moverow:	.res	1
+movestep:	.res	1
+stepdone:	.res	1
+fromidx:	.res	1
+toidx:		.res	1
 
 .code
 
 board_init:
 		lda	#$0
+		sta	moving
 		ldx	#$f
 clearloop:	sta	board,x
 		dex
@@ -52,4 +71,127 @@ ba_scannext:	lda	board,x
 ba_add4:	lda	#$2
 ba_done:	ldx	tmppos
 		sta	board,x
+		clc
+		rts
+
+board_up:
+		lda	#DIR_UP
+		bne	board_setdir
+
+board_down:
+		lda	#DIR_DOWN
+		bne	board_setdir
+
+board_left:
+		lda	#DIR_LEFT
+		bne	board_setdir
+
+board_right:
+		lda	#DIR_RIGHT
+		
+board_setdir:	sta	direction
+		lda	#$1
+		sta	moving
+		lda	#$0
+		ldx	#$3
+initcombined:	sta	combined,x
+		dex
+		bpl	initcombined
+		rts
+
+
+getidx:
+		lda	direction
+		cmp	#DIR_RIGHT
+		beq	getidx_right
+		cmp	#DIR_LEFT
+		beq	getidx_left
+		cmp	#DIR_DOWN
+		beq	getidx_down
+
+getidx_up:	
+		lda	#$3
+		sbc	movestep
+		asl	a
+		asl	a
+		adc	moverow
+		sta	toidx
+		adc	#$4
+		sta	fromidx
+		rts
+
+getidx_down:
+		lda	movestep
+		asl	a
+		asl	a
+		adc	moverow
+		sta	fromidx
+		adc	#$4
+		sta	toidx
+		rts
+
+getidx_left:
+		lda	moverow
+		asl	a
+		asl	a
+		adc	#$3
+		sbc	movestep
+		tax
+		stx	toidx
+		inx
+		stx	fromidx
+		rts
+
+getidx_right:
+		lda	moverow
+		asl	a
+		asl	a
+		adc	movestep
+		tax
+		stx	fromidx
+		inx
+		stx	toidx
+		rts	
+
+board_step:
+		lda	#$0
+		sta	stepdone
+		lda	#$3
+		sta	moverow
+bs_rowloop:	lda	#$2
+		sta	movestep
+bs_steploop:	jsr	getidx
+		ldx	fromidx
+		lda	board,x
+		sta	tmppos
+		beq	bs_stepnext
+		ldx	toidx
+		lda	board,x
+		bne	bs_checkcomb
+		lda	tmppos
+		sta	board,x
+		lda	#$0
+		ldx	fromidx
+		sta	board,x
+		inc	stepdone
+		bne	bs_stepnext
+bs_checkcomb:	ldx	moverow
+		ldy	combined,x
+		bne	bs_stepnext
+		cmp	tmppos
+		bne	bs_stepnext
+		lda	#$0
+		ldx	fromidx
+		sta	board,x
+		ldx	toidx
+		inc	board,x
+		inc	stepdone
+		ldx	moverow
+		inc	combined,x
+bs_stepnext:	dec	movestep
+		bpl	bs_steploop
+		dec	moverow
+		bpl	bs_rowloop
+		lda	#$0
+		cmp	stepdone
 		rts
--- src/main.s
+++ src/main.s
@@ -6,6 +6,10 @@
 .include "screen.inc"
 .include "board.inc"
 
+.zeropage
+
+validmove:	.res	1
+
 .segment "MAIN"
 
 		sei
@@ -20,8 +24,44 @@
 
 		jsr	screen_draw
 
-end:		jsr	js_get
-		bcs	end
-		inc	$d020
-		bcc	end
+check_js:	jsr	js_get
+		bcs	check_js
+		lsr	a
+		bcs	move_up
+		lsr	a
+		bcs	move_down
+		lsr	a
+		bcs	move_left
+		lsr	a
+		bcs	move_right
+		bcc	check_js
+
+move_up:	jsr	board_up
+		bne	dosteps
+
+move_down:	jsr	board_down
+		bne	dosteps
+
+move_left:	jsr	board_left
+		bne	dosteps
+
+move_right:	jsr	board_right
+
+dosteps:	lda	#$0
+		sta	validmove
+steploop:	jsr	board_step
+		bcs	stepdone
+		lda	#$1
+		sta	validmove
+		jsr	screen_draw
+		bne	steploop
+stepdone:	lda	validmove
+		beq	check_js
+		jsr	board_addpiece
+		bcc	add_done
+
+gameover:	bcs	gameover
+
+add_done:	jsr	screen_draw
+		bne	check_js
 
